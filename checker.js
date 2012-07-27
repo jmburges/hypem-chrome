e6a54d34053c70142cab48f66a83f32dc8f78b1d
Joe Burgess <joemburgess@gmail.com>
2012-07-27 15:01:05 -0400

    Made Loving work

--- checker.js
+++ checker.js
@@ -4,15 +4,11 @@ if (document.defaultView.getComputedStyle(document.getElementById('player-contai
 else {
 	var div = document.getElementById("player-nowplaying");
 	var string = div.childNodes[0].innerHTML + " - " + div.childNodes[2].innerHTML;
-	var loved="fav-off"
 	playbutton = document.getElementById("playerPlay");
-	var re = new RegExp(/fav_item_/);
-	for(i=0;i<div.childNodes.length;i++){
-	var m = re.test(div.childNodes[i].id);
-	if (m){
-		loved=div.childNodes[i].className;
-	}
-	}
+
+	div = document.getElementById("playerFav");
+var re = new RegExp(/fav-[onf]{2,3}/);
+var loved = re.exec(div.className);
 	chrome.extension.sendRequest({"exists": string, "playpause": playbutton.className, "loved": loved});
 
 		
--- next.js
+++ next.js
@@ -3,15 +3,8 @@ var e = document.createEvent('MouseEvents');
     e.initEvent( 'click', true, true );
     a.dispatchEvent(e);
 
-	var div = document.getElementById("player-nowplaying");
-	var loved="fav-off"
-	var string = div.childNodes[0].innerHTML + " - " + div.childNodes[2].innerHTML;
-	var re = new RegExp(/fav_item_/);
-	for(i=0;i<div.childNodes.length;i++){
-	var m = re.test(div.childNodes[i].id);
-	if (m){
-		loved=div.childNodes[i].className;
-	}
-	}
+	var div = document.getElementById("playerFav");
+var re = new RegExp(/fav-[onf]{2,3}/);
+var loved = re.exec(div.className);
 	chrome.extension.sendRequest({nowplaying: string, "loved": loved});
 	
