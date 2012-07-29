af6bc3f279d4b6e41859f87a562e282f3b99e077
Joe Burgess <joemburgess@gmail.com>
2012-07-29 07:43:47 -0400

    Fixed the names not showing up in popup and removed content script.

--- prev.js
+++ prev.js
@@ -3,8 +3,10 @@ var e = document.createEvent('MouseEvents');
     e.initEvent( 'click', true, true );
     a.dispatchEvent(e);
 	
-		
-  var div = document.getElementById("playerFav");
+	 	
+	var div = document.getElementById("player-nowplaying");
+	var string = div.childNodes[0].innerHTML + " - " + div.childNodes[2].innerHTML;
+  div = document.getElementById("playerFav");
   var re = new RegExp(/fav-[onf]{2,3}/);
   var loved = re.exec(div.className);
 	chrome.extension.sendRequest({nowplaying: string, "loved": loved});;
